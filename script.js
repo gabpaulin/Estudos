979489c50341eeb40faf3fb1c63355ac58421e11
Gabriel Stoltemberg <stoltemberg.stoltemberg@gmail.com>
2023-12-08 01:22:36 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
-  
+  html.classList.toggle("light")
+
   const img = document.querySelector("#profile img")
-  if (html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar2.png')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar2.png")
   } else {
-    img.setAttribute('src', './assets/avatar1.png')
+    img.setAttribute("src", "./assets/avatar1.png")
   }
 }
